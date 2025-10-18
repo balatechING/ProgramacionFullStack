d90eba550808afa162e1d8da89d1a98d010e44f7
Buseano777 <enanitoemexx@gmail.com>
2025-10-17 22:42:21 -0300

    Agregó js para generar dinos aleatoriamente

---
+++ JS/dinos.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const dinosContainer = document.querySelector(".Dinos_ALM");
+    const btnMostrar = document.getElementById("DinoGen");
+
+    btnMostrar.addEventListener("click", () => {
+        dinosContainer.innerHTML = "";
+
+        const dinos = [
+            "../Recursos/Dino1.png",
+            "../Recursos/Dino2.png",
+            "../Recursos/Dino3.png",
+            "../Recursos/Dino4.png",
+            "../Recursos/Dino5.png",
+            "../Recursos/Dino6.png"
+        ];
+
+        const cantidad = 6;
+
+        for (let i = 0; i < cantidad; i++) {
+
+            const randomIndex = Math.floor(Math.random() * dinos.length);
+            const src = dinos[randomIndex];
+
+            const img = document.createElement("img");
+            img.src = src;
+            img.alt = "Dinosaurio";
+            img.draggable = true;
+            dinosContainer.appendChild(img);
+        }
+
+        if (typeof activarDragDrop === "function") {
+            activarDragDrop();
+        }
+    });
+});
